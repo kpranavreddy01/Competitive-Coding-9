b3f2ba9ad46bf032032643bd13bf0e5e7593121d
kpranavreddy01 <114799260+kpranavreddy01@users.noreply.github.com>
2024-04-13 12:12:40 +0530

    problems solved

---
+++ maxticket.java
@@ -0,0 +1,25 @@
+// Time Complexity : O(365)
+// Space Complexity : O(365)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+class Solution {
+	public int mincostTickets(int[] days, int[] costs) {
+		int[] dp = new int[366];
+		int j = 0;
+		for (int i = 1; i < dp.length; i++) {
+			if (j == days.length)
+				break;
+			if (days[j] != i)
+				dp[i] = dp[i - 1];
+			else {
+				int day1 = i - 1 > 0 ? costs[0] + dp[i - 1] : costs[0];
+				int day7 = i - 7 > 0 ? costs[1] + dp[i - 7] : costs[1];
+				int day30 = i - 30 > 0 ? costs[2] + dp[i - 30] : costs[2];
+
+				dp[i] = Math.min(day1, Math.min(day7, day30));
+				j++;
+			}
+		}
+		return dp[days[days.length - 1]];
+	}
+}
